4d55420351fc18e89232768808956b9390ffee40
Vago16 <vagelispetropoulos@yahoo.com>
2025-02-01 18:39:19 -0500

    finished assignment 8

--- computing-essentials/module3/AHPA #8 - Titration Time!.py
+++ computing-essentials/module3/AHPA #8 - Titration Time!.py
@@ -23,6 +23,22 @@
 # Notify the student when the mixture turns 
 # blue.
 #
-# Student Name: Evagelos
+# Student Name: Evagelos Petropoulos
 
-#
+curr_amount = 0
+add_amount = 0.5
+button_press = 0
+
+end_amount = int(input('How many mls (max 18) should the mixture require to turn blue? '))
+
+while (curr_amount < end_amount):
+    button_press += 1   #increment to show how many times button has been presses
+    curr_amount += add_amount
+#    print(curr_amount, add_amount) used in testing to make sure values were adding up correctly
+    if (curr_amount > 18):
+        print('Solution did not turn blue before 18 mls was added.')
+        break
+    if (curr_amount == end_amount):
+        print('Suolution turned blue.')
+
+print(f'Button was pressed {button_press} times.')
