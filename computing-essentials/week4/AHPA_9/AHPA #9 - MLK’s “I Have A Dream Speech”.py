eefa5a26dbe9bec485aaac6494b2635585b363d6
Vago16 <vagelispetropoulos@yahoo.com>
2025-02-05 14:24:56 -0500

    initialized AHPA 9

---
+++ computing-essentials/week4/AHPA_9/AHPA #9 - MLK’s “I Have A Dream Speech”.py
@@ -0,0 +1,12 @@
+# AHPA #9 - MLK’s “I Have A Dream Speech”
+#
+# Print the total number of
+# characters in the speech.
+#
+# Open another file for output.
+#
+# Write only the first paragraph of
+# this speech to that file.
+#
+# Student Name: Evagelos Petropoulos
+#
