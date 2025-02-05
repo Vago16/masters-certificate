5ec4eb486df54f43896e009a83ebd6294e816af0
Vago16 <vagelispetropoulos@yahoo.com>
2025-02-05 14:37:03 -0500

    finished first part of AHPA 9

--- computing-essentials/week4/AHPA_9/AHPA #9 - MLK’s “I Have A Dream Speech”.py
+++ computing-essentials/week4/AHPA_9/AHPA #9 - MLK’s “I Have A Dream Speech”.py
@@ -10,3 +10,18 @@
 #
 # Student Name: Evagelos Petropoulos
 #
+
+infile = open('AHPA #9 - MLK Speech.txt', 'r')
+
+num_chars = 0
+
+while True:
+    char = infile.read(1)
+    num_chars += 1
+    if not char:
+        break
+
+print(num_chars)
+    
+
+infile.close()
