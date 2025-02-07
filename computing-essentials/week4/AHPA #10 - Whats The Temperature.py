dea86f44de97fd5846cef15fa82960e3530bd5b0
Vago16 <vagelispetropoulos@yahoo.com>
2025-02-07 16:23:55 -0500

    added comments

--- computing-essentials/week4/AHPA #10 - Whats The Temperature.py
+++ computing-essentials/week4/AHPA #10 - Whats The Temperature.py
@@ -19,14 +19,18 @@
 
 def conversion():
     type_of_temp = input('What type of temperature are you entering, Farenheit or Celsius? Enter F or C. ')
-    temperature = float(input('What degree is the temperature? '))
-    if type_of_temp.upper() == 'F':
-        temperature = (temperature - 32) / 1.8
+    if type_of_temp.upper() == 'F':     #upper() changes the case of whats entered so we do not have to specify all cases
+        temperature = float(input('What degree is the temperature? '))      #temperature's type is changed to float so calculations can be done
+        temperature = (temperature - 32) / 1.8      #changes type of entered value to the other type of degrees
         type_of_temp = 'Celsius'
+        print('Your converted temperature is {0:.2f} degrees {1}.'.format(temperature, type_of_temp))       #prints out statment with all necessary info
     elif type_of_temp.upper() == 'C':
+        temperature = float(input('What degree is the temperature? '))
         temperature = (temperature * 1.8) + 32
         type_of_temp = 'Farenheit'
+        print('Your converted temperature is {0:.2f} degrees {1}.'.format(temperature, type_of_temp))
+    else:
+        print('Please specify Farenheit or Celsius.')       #in case anything other than 'F' or 'C' are entered, the program will not proceed further
     
-    print('Your converted temperature is {0:.2f} degrees {1}.'.format(temperature, type_of_temp))
-
+   
 conversion()
