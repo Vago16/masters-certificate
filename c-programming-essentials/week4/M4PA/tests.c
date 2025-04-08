9efd762f50374dbbf0386f293c2108d8992f1fe1
Vago16 <vagelispetropoulos@yahoo.com>
2025-04-07 20:10:54 -0700

    finished tools.c and test.c

--- c-programming-essentials/week4/M4PA/tests.c
+++ c-programming-essentials/week4/M4PA/tests.c
@@ -4,25 +4,114 @@
 #include "tools.h"
 #include "testlib.h"
 
-void test_is_sorted_1(){
+//test_sort() tests are to check if just the function matrixSort works
+//test_is_sorted() tests are to check if the function isMatrixSorted works too
+
+void example_test(){
     // Example test function
-    int matrix[NB_ROWS][NB_COLS] = {{1 , 2 , 3 , 4 ,  5} ,
-                                    {6 , 7 , 8 , 9 , 10} ,
+    int matrix[NB_ROWS][NB_COLS] = {{1 , 2 , 3 , 4 ,  5},
+                                    {6 , 7 , 8 , 9 , 10},
                                     {11, 12, 13, 14, 15} };
     TEST("Already sorted matrix", isMatrixSorted(matrix) == 1);
 }
 
 void test_sort_1(){
     // Example test function
-    int matrix[NB_ROWS][NB_COLS] = {{1 , 2 , 5 , 4 ,  3} ,
-                                    {6 , 7 , 8 , 9 , 10} ,
+    int matrix[NB_ROWS][NB_COLS] = {{1 , 2 , 5 , 4 ,  3},
+                                    {6 , 7 , 8 , 9 , 10},
                                     {11, 12, 13, 14, 15} };
     matrixSort(matrix);
     TEST("test_sort_1",isMatrixSorted(matrix));
 }
 
+void test_is_sorted_1(){
+    // tests if second dimension is working properly, should FAIL
+    int matrix[NB_ROWS][NB_COLS] = {{6 , 7 , 8 , 9 , 10},
+                                    {1 , 2 , 3 , 4 ,  5},
+                                    {11, 12, 13, 14, 15} };
+    TEST("test_is_sorted_1",isMatrixSorted(matrix));
+}
+
+void test_is_sorted_2(){
+    // tests with negative numbers, sorted wrong in both columns and rows, should FAIL
+    int matrix[NB_ROWS][NB_COLS] = {{-1, -2, -3, -4, -5},
+                                    {-6, -7, -8, -9, -10},
+                                    {-11, -12, -13, -14, -15} };
+    TEST("test_is_sorted_2",isMatrixSorted(matrix));
+}
+
+void test_is_sorted_3(){
+    // tests with negative numbers, sorted in rows but not in columns, should FAIL
+    int matrix[NB_ROWS][NB_COLS] = {{-5, -4, -3, -2, -1},
+                                    {-10, -9, -8, -7, -6},
+                                    {-15, -14, -13, -12, -11} };
+    TEST("test_sort_3",isMatrixSorted(matrix));
+}
+
+void test_is_sorted_4(){
+    // tests with negative numbers, sorted both in columns and rows
+    int matrix[NB_ROWS][NB_COLS] = {{-15, -14, -13, -12, -11},
+                                    {-10, -9, -8, -7, -6},
+                                    {-5, -4, -3, -2, -1} };
+    TEST("test_is_sorted_4",isMatrixSorted(matrix));
+}
+
+void test_sort_2(){
+    // tests with negative numbers, sorted both in columns and rows, but for matrixSort this time too
+    int matrix[NB_ROWS][NB_COLS] = {{-15, -14, -13, -12, -11},
+                                    {-10, -9, -8, -7, -6},
+                                    {-5, -4, -3, -2, -1} };
+    matrixSort(matrix);
+    TEST("test_sort_2",isMatrixSorted(matrix));
+}
+
+void test_is_sorted_5(){
+    // tests with duplicate numbers and out of order, should FAIL
+    int matrix[NB_ROWS][NB_COLS] = {{1, 2, 5, 2, 3},
+                                    {11, 11, 13, 15, 15},
+                                    {6, 7, 8, 6, 10} };
+    TEST("test_is_sorted_5",isMatrixSorted(matrix));
+}
+
+void test_sort_3(){
+    // tests matrixSort with duplicate numbers
+    int matrix[NB_ROWS][NB_COLS] = {{1, 2, 5, 2, 3},
+                                    {11, 11, 13, 15, 15},
+                                    {6, 7, 8, 6, 10} };
+    matrixSort(matrix);
+    TEST("test_sort_3",isMatrixSorted(matrix));
+}
+
+void test_sort_4(){
+    // tests matrixSort with all equal numbers
+    int matrix[NB_ROWS][NB_COLS] = {{7, 7, 7, 7, 7},
+                                    {7, 7, 7, 7, 7},
+                                    {7, 7, 7, 7, 7} };
+    matrixSort(matrix);
+    TEST("test_sort_4",isMatrixSorted(matrix));
+}
+
+void test_sort_5(){
+    // tests an already sorted 2D array,like example_test(), but with larger values
+    int matrix[NB_ROWS][NB_COLS] = {{10 , 20, 30, 40, 50},
+                                    {60, 70, 80, 90, 100},
+                                    {1000, 2000, 3000, 4000, 5000} };
+    matrixSort(matrix);
+    TEST("test_sort_5",isMatrixSorted(matrix));
+}
+
+
 void runAllTests(){
     // This is how you would use the above test functions
-    // test_is_sorted_1();
-    // test_sort_1();
+    example_test();
+    test_sort_1();
+    test_is_sorted_1();
+    test_is_sorted_2();
+    test_is_sorted_3();
+    test_is_sorted_4();
+    test_sort_2();
+    test_is_sorted_5();
+    test_sort_3();
+    test_sort_4();
+    test_sort_5();
 }
--- c-programming-essentials/week4/M4PA/tools.c
+++ c-programming-essentials/week4/M4PA/tools.c
@@ -4,9 +4,35 @@
 #include "tools.h"
 
 int isMatrixSorted( int data[NB_ROWS][NB_COLS] ){
-    return 0;  // modify this
+    int total_elements = NB_ROWS * NB_COLS; //size of array
+
+    for (int i = 0; i < total_elements - 1; i++) {
+        int current = data[i / NB_COLS][i % NB_COLS];   //first index is referencing row, second index is referencingcolumn
+        int next = data[(i + 1) / NB_COLS][(i + 1) % NB_COLS];      //next elements in row and column, respectively
+        if (current > next) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 void matrixSort( int data[NB_ROWS][NB_COLS]){
-    return; // modify this
+    int i, j;       //initialize loop variables
+    int total_elements = NB_ROWS * NB_COLS; //size of array
+
+    for (i = 0; i < total_elements - 1; i++) {
+
+        for (j = i + 1; j < total_elements; j++) {
+            int iRow = i / NB_COLS;     //variables for outer loop row and column
+            int iCol = i % NB_COLS;
+            int jRow = j / NB_COLS;     //variables for inner loop row and column
+            int jCol = j % NB_COLS;
+
+            if (data[iRow][iCol] > data[jRow][jCol]) {
+                int temp = data[iRow][iCol];        //temporarary variable to hold data to swap
+                data[iRow][iCol] = data[jRow][jCol];
+                data[jRow][jCol] = temp;
+            }
+        }
+    }
 }
