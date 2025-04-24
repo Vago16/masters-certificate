f49bdbe478a4793fbccf1db307fe09396cd2f232
Vago16 <vagelispetropoulos@yahoo.com>
2025-04-24 17:40:20 -0400

    initialized M6A01

---
+++ c-programming-essentials/week6/M6A01/hsl.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "hsl.h"
+
+int hsl_length ( char* s ) {
+
+}
+
+bool hsl_compare ( char* s1 , char* s2 ) {
+
+}
+
+char * hsl_allocate ( int size ){
+
+}
+
+bool hsl_deallocate (char* s){
+
+}
+
+bool hsl_deallocate_cleanly(char* s){
+
+}
+
+char * hsl_clone ( char* s ){
+
+}
+
+char* hsl_gather (char* s1 , char* s2 ){
+
+}
+
+char* hsl_extract ( char* s1 , int start , int end ){
+    
+}
