3442b4be2855935fe8a09270513bb867f9b3ca22
Vago16 <vagelispetropoulos@yahoo.com>
2025-04-23 22:59:33 -0400

    finished setting up majority of class and other code needed

---
+++ data-structures-essentials/week6/project4/p4.py
@@ -0,0 +1,131 @@
+from timeit import default_timer as timer
+import sys
+
+#start timer
+start = timer()
+
+
+####Hash Table Class####
+
+class HashTable:
+    def __init__(self, size = 29, a = 31):
+        self.size = size        #given table sie
+        self.a = a      #constant for hash function
+        self.table = [""] * self.size
+
+    def __len__(self):
+        return self.size
+
+    def hash_function(self, key):
+        return hash(key) % self.size
+
+    #insert values into hash table
+    def insert(self, key):
+        key = key.lower()       #to make sure only lowercase values are stored
+        index = self.hash_function(key)
+        orig_index = index      #index before insertion
+        i = 0
+
+        while self.table[index] not in ("", key):
+            index = (index + 1) % self.size
+            i += 1
+
+            if index == orig_index:
+                raise Exception("Table is full.")
+            
+        if self.table[index] == "":
+            self.table[index] = key
+            #uncomment to make sure insert is working correctly
+            ##print("inserted {}".format(index))
+
+    #print hash table for testing
+    def display(self):
+        i = 0
+        while i < self.size:
+            if self.table[i] != "":
+                print("Index {1}: {0}".format(self.table[i], i))
+            i += 1
+
+    #obtain the hash code for a given word
+    def _poly_hash_code(self, key):
+        n = len(key)
+        mask = ( 1 << 32 ) - 1      #limits to 32 bit ints
+        h = 0       #initialize hash code
+        for char in range(n):
+            h = ( h << 5 & mask ) | ( h >> 27 )     #5 bit cyclic shift
+            h += ord(key[char]) * (self.a ** (n - 1 - char))
+        return h
+
+    #division method as the compression function
+    def hash_function(self, key):
+        return self._poly_hash_code(key) % self.size
+    
+    #checks if word from second file exists in the hash table
+    def contains(self, key):
+        index = self.hash_function(key)
+        orig_index = index 
+
+        while self.table[index] != "":
+            if self.table[index] == key:
+                return True
+            index = (index + 1) % self.size
+            if index == orig_index:
+                break
+        return False
+
+
+
+
+#command line arguments
+if len(sys.argv) != 3:
+    raise ValueError('Please provide two file names.')
+firstFile = sys.argv[1]
+secondFile = sys.argv[2]
+print("\nThe hash table will be built from:", firstFile)
+print("\nThe hash table will be compared to:", secondFile)
+
+#open and read first file, and build hash table
+first = open(firstFile,"r")	#open provided text file
+firstList = first.readlines()	#read all lines in text file
+first.close()	
+
+#initialize first hash table
+first_hash_table = HashTable()
+
+
+for line in firstList:
+    words = line.strip().lower().split()
+    for word in words:
+        first_hash_table.insert(word)
+
+first_hash_table.display()
+
+#open and read second file
+second = open(secondFile,"r")	#open provided text file
+secondList = second.readlines()	#read all lines in text file
+second.close()	
+
+#initialize the number of lines to be read and later displayed
+num_lines = 0
+
+#initialize the number of words to be read and later displayed
+num_words = 0
+
+for line in secondList:
+    num_lines += 1
+    words = line.strip().lower().split()
+    for word in words:
+        num_words += 1
+
+print("**********************")
+print("***** Statistics *****")
+print("**********************")
+print("Break down by keyword:")
+print("Total lines read: {0}".format(num_lines))
+print("Total words read: {0}".format(num_words))
+#end timer
+end = timer()
+
+#print program runtime
+print("Total Time of Program:", (end - start) * 1000, "milliseconds")
+
