e02ec160d927529c3642705ec7183c74fcd4f9c4
Vago16 <vagelispetropoulos@yahoo.com>
2025-07-30 16:42:14 -0400

    updated project

--- network-essentials/project/weblite_knock.c
+++ network-essentials/project/weblite_knock.c
@@ -14,7 +14,7 @@ int main() {
     return 0;
 }
 
-int wait_for_knock() {      //reusing some code from xServer.c
+int wait_for_knock() {      //reusing code from xServer.c
     int                  server_s;        // Server socket descriptor
     struct sockaddr_in   server_addr;     // Server Internet address
     struct sockaddr_in   client_addr;     // Client Internet address
@@ -28,6 +28,36 @@ int wait_for_knock() {      //reusing some code from xServer.c
     time_t last_knock_time = 0;
 
     while (curr_knock < 3) {
-        
+        //create socket, taken from xServer.c
+        server_s = socket(AF_INET, SOCK_DGRAM, 0);      
+        if (server_s < 0) {
+                printf("*** ERROR - socket() failed \n");
+                exit(-1);
+            }
+        //fill in socket's address information
+        memset(&server_addr, 0, sizeof(server_addr));       //initialize struct to 0
+        server_addr.sin_family = AF_INET;                 // Address family to use
+        server_addr.sin_port = htons(knock_seq[curr_knock]);           // Port number to use
+        server_addr.sin_addr.s_addr = htonl(INADDR_ANY);  // Listen on any IP address
+        retcode = bind(server_s, (struct sockaddr *)&server_addr,
+        sizeof(server_addr));
+        if (retcode < 0) {
+            printf("*** ERROR - bind() failed \n");
+            exit(-1);
+            }
+
+        // Wait to receive a message from client
+        printf("Waiting for recvfrom() to complete... \n");
+        addr_len = sizeof(client_addr);
+        retcode = recvfrom(server_s, in_buf, sizeof(in_buf), 0,
+            (struct sockaddr *)&client_addr, &addr_len);
+        if (retcode < 0) {
+            printf("*** ERROR - recvfrom() failed \n");
+            close(server_s);       //deallocate memory location
+            exit(-1);
+        }
+
+        //checking elapsed time
+        time_t curr_time = time(NULL);      //initialize current time
     }
 }
