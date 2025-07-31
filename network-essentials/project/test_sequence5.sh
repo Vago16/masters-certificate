d49b5cf7fef7a4635d774443a5a6b663313afefb
Vago16 <vagelispetropoulos@yahoo.com>
2025-07-30 21:09:37 -0400

    updated project

--- network-essentials/project/test_sequence5.sh
+++ network-essentials/project/test_sequence5.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
-echo "=== Test Sequence #5 ==="
+#ChatGpt(version GPT-4.1)was used to convert the bash test files into .sh files able to executed by a Linux/Unix system
+#I first asked the model "Can you make these batch files into .sh files executable by a mac?" followed by giving it the project instructions,
+#and then asking it to replace the ./udpMsgSend function with the netcat function since udpMsgSend is not native to Mac.
+#Link-----
+#     https://chatgpt.com/c/688abd23-2b98-800e-b768-f38a0f0efaa5
+echo "REM Test sequence #5 for CNT 5008 project"
 
 curl http://127.0.0.1:8080/test.html
 
 echo -n "Mickey" | nc -u -w1 127.0.0.1 1100
+echo "Waiting for 0 seconds, press CTRL+C to quit ..."
 sleep 2
 
 echo -n "Minnie" | nc -u -w1 127.0.0.1 1200
+echo "Waiting for 0 seconds, press CTRL+C to quit ..."
 sleep 6
 
 echo -n "Goofy" | nc -u -w1 127.0.0.1 1300
+echo "Waiting for 0 seconds, press CTRL+C to quit ..."
 sleep 2
 
 echo -n "Mickey" | nc -u -w1 127.0.0.1 1100
+echo "Waiting for 0 seconds, press CTRL+C to quit ..."
 sleep 2
 
 echo -n "Minnie" | nc -u -w1 127.0.0.1 1200
+echo "Waiting for 0 seconds, press CTRL+C to quit ..."
 sleep 2
 
 echo -n "Goofy" | nc -u -w1 127.0.0.1 1300
+echo "Waiting for 0 seconds, press CTRL+C to quit ..."
 sleep 2
 
 curl http://127.0.0.1:8080/test.html
--- network-essentials/project/weblite_knock.c
+++ network-essentials/project/weblite_knock.c
@@ -11,7 +11,7 @@ int wait_for_knock();      //prototype for knock function
 
 int main() {
     printf("Waiting for knock sequence...\n");
-    if (wait_for_knock) {
+    if (wait_for_knock()) {
     run_server();
     }
     return 0;
@@ -52,8 +52,10 @@ int wait_for_knock() {      //reusing code from xServer.c
         // Wait to receive a message from client
         printf("Waiting for recvfrom() to complete... \n");
         addr_len = sizeof(client_addr);
+        printf("Current knock: %d (port %d)\n", curr_knock, knock_seq[curr_knock]);
         retcode = recvfrom(server_s, in_buf, sizeof(in_buf), 0,
             (struct sockaddr *)&client_addr, &addr_len);
+        printf("Knock received on port %d\n", knock_seq[curr_knock]);
         if (retcode < 0) {
             printf("*** ERROR - recvfrom() failed \n");
             close(server_s);       //deallocate memory location
@@ -62,7 +64,7 @@ int wait_for_knock() {      //reusing code from xServer.c
 
         //checking elapsed time
         time_t curr_time = time(NULL);      //initialize current time
-        if ((curr_knock > 0) && difftime(curr_time, last_knock_time)) {
+        if ((curr_knock > 0) && difftime(curr_time, last_knock_time) > KNOCK_TIMING) {
             printf("Timeout\n");
             curr_knock = 0;
         } else {
